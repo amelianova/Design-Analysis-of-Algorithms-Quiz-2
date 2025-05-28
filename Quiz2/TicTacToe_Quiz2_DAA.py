6233e790749dca828ac839d0c41956908bf3f114
amelianova <162030080+amelianova@users.noreply.github.com>
2025-05-28 20:23:05 +0700

    Update TicTacToe_Quiz2_DAA.py

--- Quiz2/TicTacToe_Quiz2_DAA.py
+++ Quiz2/TicTacToe_Quiz2_DAA.py
@@ -29,3 +29,29 @@ SQUARE_SIZE = SCREEN_WIDTH // BOARD_COLS
 # create an empty game board
 def create_board():
     return [[" " for _ in range(BOARD_COLS)] for _ in range(BOARD_ROWS)]
+
+# displays a game board on the screen with dividing lines between the squares
+def draw_lines(screen):
+    for row in range(1, BOARD_ROWS):
+        pygame.draw.line(screen, LINE_COLOR, (0, row * SQUARE_SIZE), (SCREEN_WIDTH, row * SQUARE_SIZE), LINE_WIDTH)
+    for col in range(1, BOARD_COLS):
+        pygame.draw.line(screen, LINE_COLOR, (col * SQUARE_SIZE, 0), (col * SQUARE_SIZE, SCREEN_HEIGHT), LINE_WIDTH)
+
+# draw "X" and "O" symbols on the board based on the game status
+def draw_figures(screen, board):
+    for row in range(BOARD_ROWS):
+        for col in range(BOARD_COLS):
+            if board[row][col] == "X":
+                pygame.draw.line(screen, (255, 0, 0), (col * SQUARE_SIZE + 20, row * SQUARE_SIZE + 20), 
+                                 (col * SQUARE_SIZE + SQUARE_SIZE - 20, row * SQUARE_SIZE + SQUARE_SIZE - 20), CROSS_WIDTH)
+                pygame.draw.line(screen, (255, 0, 0), (col * SQUARE_SIZE + SQUARE_SIZE - 20, row * SQUARE_SIZE + 20), 
+                                 (col * SQUARE_SIZE + 20, row * SQUARE_SIZE + SQUARE_SIZE - 20), CROSS_WIDTH)
+            elif board[row][col] == "O":
+                pygame.draw.circle(screen, CIRCLE_COLOR, (col * SQUARE_SIZE + SQUARE_SIZE // 2, row * SQUARE_SIZE + SQUARE_SIZE // 2), 
+                                   CIRCLE_RADIUS, CIRCLE_WIDTH)
+
+# display message on screen
+def draw_text(screen, text, size, color, x, y):
+    font = pygame.font.Font(None, size)
+    label = font.render(text, True, color)
+    screen.blit(label, (x - label.get_width() // 2, y - label.get_height() // 2))
